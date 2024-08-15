2781f9d748d8e0016a707a5c2189341e3a8c596d
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-15 00:52:26 -0600

    feat: header

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import "normalize.css";
 import './src/scss/index.scss';
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector(".nav");
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active_burger');
+  navMenu.classList.toggle('active_nav')
+});
