2bbd30f340d54c4923c8ef5d05c6e46ae63661e6
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-17 17:00:18 -0600

    feat: main start

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active_burger');
   navMenu.classList.toggle('active_nav')
 });
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+      header.classList.add('start_scroll');
+  } else {
+      header.classList.remove('start_scroll');
+  }
+});
