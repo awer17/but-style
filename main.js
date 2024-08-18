5dc0782ac903ca6f6ef574577eaa68460a40c3b6
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-17 20:05:37 -0600

    feat: animac-bg

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ import './src/scss/index.scss';
 const burgerMenu = document.querySelector('.burger-menu');
 const navMenu = document.querySelector(".nav");
 const containerHeader = document.querySelector(".container-header")
+const body = document.querySelector('body')
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active_burger');
   navMenu.classList.toggle('active_nav')
   containerHeader.classList.toggle('active')
+  body.classList.toggle('active_burger')
 });
 
 window.addEventListener('scroll', () => {
