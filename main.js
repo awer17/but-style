ed22ad873819207643826ff97f44b4214384fbb6
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-17 18:59:39 -0600

    feat: hover

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ import './src/scss/index.scss';
 
 const burgerMenu = document.querySelector('.burger-menu');
 const navMenu = document.querySelector(".nav");
+const containerHeader = document.querySelector(".container-header")
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active_burger');
   navMenu.classList.toggle('active_nav')
+  containerHeader.classList.toggle('active')
 });
 
 window.addEventListener('scroll', () => {
