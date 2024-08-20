53190032e1b3ff5d98e9ac2d864dd84e1eea28ec
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-19 23:47:28 -0600

    feat: bg

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ import './src/js/animacia_about';
 import './src/js/slider';
 import './src/js/slider-rev';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const heroSection = document.querySelector('.hero');
+  let currentScale = 120; // Начальный масштаб фона в процентах
+  const targetScale = 128; // Конечный масштаб фона
+  const duration = 4000; // Длительность анимации в миллисекундах
+  const interval = 1000 / 60; // Время между кадрами для 60fps
+  const increment = (targetScale - currentScale) / (duration / interval); // Шаг увеличения
+
+  const zoomInBackground = () => {
+    if (currentScale < targetScale) {
+      currentScale += increment;
+      heroSection.style.backgroundSize = `auto ${currentScale}%`;
+      requestAnimationFrame(zoomInBackground);
+    }
+  };
+
+  // Запускаем анимацию при загрузке страницы
+  zoomInBackground();
+});
+
 
 const burgerMenu = document.querySelector('.burger-menu');
 const navMenu = document.querySelector(".nav");
