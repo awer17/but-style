977cc8851a86dba6324e185a25400ce57202d7db
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-08-25 20:19:11 -0600

    feat: footer

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ import './src/js/animacia_about';
 import './src/js/slider';
 import './src/js/slider-rev';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const heroSection = document.querySelector('.hero');
-  let currentScale = 120; // Начальный масштаб фона в процентах
-  const targetScale = 128; // Конечный масштаб фона
-  const duration = 4000; // Длительность анимации в миллисекундах
-  const interval = 1000 / 60; // Время между кадрами для 60fps
-  const increment = (targetScale - currentScale) / (duration / interval); // Шаг увеличения
+// document.addEventListener('DOMContentLoaded', () => {
+//   const heroSection = document.querySelector('.hero');
+//   let currentScale = 120; // Начальный масштаб фона в процентах
+//   const targetScale = 125; // Конечный масштаб фона
+//   const duration = 10000; // Длительность анимации в миллисекундах
+//   const interval = 5000 / 32; // Время между кадрами для 60fps
+//   const increment = (targetScale - currentScale) / (duration / interval); // Шаг увеличения
 
-  const zoomInBackground = () => {
-    if (currentScale < targetScale) {
-      currentScale += increment;
-      heroSection.style.backgroundSize = `auto ${currentScale}%`;
-      requestAnimationFrame(zoomInBackground);
-    }
-  };
+//   const zoomInBackground = () => {
+//     if (currentScale < targetScale) {
+//       currentScale += increment;
+//       heroSection.style.backgroundSize = ` ${currentScale}%`;
+//       requestAnimationFrame(zoomInBackground);
+//     }
+//   };
 
-  // Запускаем анимацию при загрузке страницы
-  zoomInBackground();
-});
+//   // Запускаем анимацию при загрузке страницы
+//   zoomInBackground();
+// });
 
 
 const burgerMenu = document.querySelector('.burger-menu');
