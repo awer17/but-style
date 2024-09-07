a6979cd03620b0658b93f53d25e02d1d14575bdf
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-09-06 21:25:26 -0600

    feat: js json  slider

--- src/js/data_json.js
+++ src/js/data_json.js
@@ -1,11 +1,28 @@
 import jsonServicse from '../json/servicss.json';
 
-console.log(jsonServicse[1].name);
-
 const swiperWrapper = document.querySelector('.swiper-wrapper');
-
 swiperWrapper.textContent = '';
 
 jsonServicse.forEach(item =>{
-  swiperWrapper.textContent += '1'
+  swiperWrapper.innerHTML += `<div class="swiper-slide">
+  <img class="img-slide"  src="src/image/services/services-slider/${item.img1}" alt="${item.img1}">
+  <div class="action-buttons">
+    <button data-btn-id="${item.code}" class="detsls-service">details</button>
+    <button class="link-request">
+      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.99 4.5L14.99 8.5M14.99 8.5L10.99 13M14.99 8.5H1.98999" stroke="#4A4A49"/>
+        </svg>
+      Request an Estimate
+    </button>
+  </div>
+  <h3>${item.name}</h3>
+</div>
+`
 })
+
+document.querySelectorAll('.detsls-service').forEach(button => {
+  button.addEventListener('click', (event) => {
+    const btnId = event.target.getAttribute('data-btn-id');
+    window.location.href = `services.html?id=${btnId}`;
+  });
+});
