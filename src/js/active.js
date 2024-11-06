43268de8706709e6cddd4b14dc4b4e6d20846d24
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2024-11-05 22:31:48 -0700

    feat: form

---
+++ src/js/active.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener('DOMContentLoaded', function (){
+  const  selectList = document.querySelectorAll('.select-list');
+  console.log(selectList)
+
+  selectList.forEach( selectList => {
+    selectList.addEventListener('focus', function () {
+      if (!selectList.classList.contains('active_select')) {
+        selectList.classList.add('active_select'); // Добавляем класс при фокусе
+      }
+    });
+  })
+})
